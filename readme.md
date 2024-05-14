044d51daab15626c23bd6bded03800d5fa6d4470
Vitaly Domnikov <dotcypress@gmail.com>
2024-05-13 19:35:58 -0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# ESP32C3 Forbidden Access Point
+
+## Flashing firmware
+
+* [Install Rust](https://rustup.rs)
+* [Setup ESP32 RISC-V target](https://esp-rs.github.io/book/installation/riscv.html)
+* Install espflash: `cargo install espflash`
+* Flash firmware: `cargo run --release`
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use crate::captive::CaptivePortal;
+use dns::*;
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::prelude::Peripherals,
+    http::{
+        server::{Configuration, EspHttpServer},
+        Method,
+    },
+    io::Write,
+    ipv4::{self, Mask, RouterConfiguration, Subnet},
+    log::EspLogger,
+    netif::{EspNetif, NetifConfiguration, NetifStack},
+    nvs::EspDefaultNvsPartition,
+    sys::{self, EspError},
+    wifi::{self, AccessPointConfiguration, EspWifi, WifiDriver},
+};
+use std::{
+    net::Ipv4Addr,
+    thread::{self, sleep},
+    time::Duration,
+};
+
+mod captive;
+mod dns;
+
+pub const IP_ADDRESS: Ipv4Addr = Ipv4Addr::new(192, 168, 55, 1);
+
+fn main() -> Result<(), EspError> {
+    unsafe {
+        sys::nvs_flash_init();
+    }
+    sys::link_patches();
+    EspLogger::initialize_default();
+
+    let event_loop = EspSystemEventLoop::take()?;
+    let peripherals = Peripherals::take()?;
+
+    let wifi_driver = WifiDriver::new(
+        peripherals.modem,
+        event_loop.clone(),
+        EspDefaultNvsPartition::take().ok(),
+    )?;
+    let mut wifi = EspWifi::wrap_all(
+        wifi_driver,
+        EspNetif::new(NetifStack::Sta)?,
+        EspNetif::new_with_conf(&NetifConfiguration {
+            ip_configuration: ipv4::Configuration::Router(RouterConfiguration {
+                subnet: Subnet {
+                    gateway: IP_ADDRESS,
+                    mask: Mask(24),
+                },
+                dhcp_enabled: true,
+                dns: Some(IP_ADDRESS),
+                secondary_dns: Some(IP_ADDRESS),
+            }),
+            ..NetifConfiguration::wifi_default_router()
+        })?,
+    )
+    .expect("WiFi init failed");
+
+    wifi.set_configuration(&wifi::Configuration::AccessPoint(
+        AccessPointConfiguration {
+            ssid: env!("SSID").into(),
+            auth_method: wifi::AuthMethod::None,
+            ..Default::default()
+        },
+    ))?;
+    wifi.start()?;
+
+    let mut dns = SimpleDns::try_new(IP_ADDRESS).expect("DNS server init failed");
+    thread::spawn(move || loop {
+        dns.poll().ok();
+        sleep(Duration::from_millis(50));
+    });
+
+    let config = Configuration::default();
+    let mut server = EspHttpServer::new(&config).expect("HTTP server init failed");
+    CaptivePortal::attach(&mut server, IP_ADDRESS).expect("Captive portal attach failed");
+
+    server.fn_handler("/", Method::Get, move |request| {
+        request
+            .into_ok_response()?
+            .write_all(include_bytes!("web/index.html"))?;
+        Ok(())
+    })?;
+
+    server.fn_handler("/rr.gif", Method::Get, move |request| {
+        request
+            .into_response(200, None, &[("Content-Type", "image/gif")])?
+            .write_all(include_bytes!("web/rr.gif"))?;
+        Ok(())
+    })?;
+
+    loop {
+        sleep(Duration::from_millis(50));
+    }
+}
